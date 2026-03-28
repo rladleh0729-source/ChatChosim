52e7288912538bfa069b33d3f04d315a990d9575
김이도 <rladleh0729@naver.com>
2026-03-28 21:06:24 +0900

    legacy 페이지 수정

--- src/main/java/com/chatchosim/LegacyController.java
+++ src/main/java/com/chatchosim/LegacyController.java
@@ -1,18 +1,18 @@
-package com.example.demo;
+package com.example.chatchosim;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LegacyController {
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/legacy";
+    }
 
     @GetMapping("/legacy")
     public String legacy() {
-        return "forward:/legacy/index.html";
-    }
-
-    @GetMapping("/legacy/")
-    public String legacySlash() {
         return "forward:/legacy/index.html";
     }
 }
